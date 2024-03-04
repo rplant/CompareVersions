b1cad0ecc2e25eda1fd3bc537ec6c0e474b1f642
John Plant <rplant@johnplant.net>
2024-03-04 18:44:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        public static int CompareVersions(string version1, string version2)
+        {
+            string[] parts1 = version1.Split('.');
+            string[] parts2 = version2.Split('.');
+
+            int maxLength = Math.Max(parts1.Length, parts2.Length);
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                int part1 = i < parts1.Length ? int.Parse(parts1[i]) : 0;
+                int part2 = i < parts2.Length ? int.Parse(parts2[i]) : 0;
+
+                if (part1 < part2)
+                {
+                    return -1;
+                }
+                else if (part1 > part2)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
